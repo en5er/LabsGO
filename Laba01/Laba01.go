d7fff9e6b3b22d135adce01704dccb0000f3bc6a
en5er <ensere90@gmail.com>
2026-09-13 22:12:31 +0400

    Проверка перехода на папку с лабой

---
+++ Laba01/Laba01.go
@@ -0,0 +1,16 @@
+package Laba01
+
+import {
+		"bufio"
+		"fmt"
+		"strconv"
+		"os"
+		"strings"
+}
+
+func average(chislo1,chislo2,chisto3 float64) float64 {
+		return (chislo1+chislo2+chisto3)/3
+}
+
+func Laba01() {
+		reader
